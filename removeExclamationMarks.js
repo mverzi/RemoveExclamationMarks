1dfcb1f98f675caf7ed1361beb5bf89d8c5c0e33
mverzi <maggie.verzi@gmail.com>
2022-11-14 10:01:28 -0500

    First commit

---
+++ removeExclamationMarks.js
@@ -0,0 +1,8 @@
+/**
+ * This function removes all exclamation marks from a given string.
+ * @param {string} s 
+ * @returns s with exclamation marks removed
+ */
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, '');
+  }
